a71d7ee8ca19bb0313a2be103e2ba2cd39341eab
Ashwini shalke <ashwinishalke050@gmail.com>
2019-07-22 23:20:52 +0530

    Modified prevButton

--- AutoLayout/AutoLayout/SwipingController.swift
+++ AutoLayout/AutoLayout/SwipingController.swift
@@ -17,13 +17,13 @@ class SwipingController : UICollectionViewController,UICollectionViewDelegateFlo
         Page(images: "bear_first", headerText: "Join use today in our fun and games!",bodyText : "Are you ready for loads and loads of fun? Don't wait any longer! We hope to see you in our stores soon."),
         Page(images: "heart_second", headerText: "Subscribe and get coupons on our daily events",bodyText : "Get notified of the savings immediately when we announce them on our website. Make sure to also give us any feedback you have."),
         Page(images: "leaf_third", headerText: "VIP members special services",bodyText : " ")
+        
     ]
     
     // controls at the bottom
     private let previousButton: UIButton = {
         let button = UIButton(type: .system)
         button.translatesAutoresizingMaskIntoConstraints = false
-        //button.backgroundColor = UIColor.darkGray
         button.setTitleColor(UIColor.darkGray, for: .normal)
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
         button.setTitle("PREV", for: .normal)
@@ -31,26 +31,25 @@ class SwipingController : UICollectionViewController,UICollectionViewDelegateFlo
         return button
     }()
     
-    @objc func HandlePrev()
-    {
-        let prevpage : Int = max (pageController.currentPage - 1 ,0)
-        let indexpath = IndexPath(item: prevpage, section: 0)
-        pageController.currentPage = prevpage
-        collectionView?.scrollToItem(at: indexpath, at: .centeredHorizontally, animated: true)
-    }
-    
-    
-    
-  private let NextButton : UIButton = {
+    private let NextButton : UIButton = {
         let button = UIButton(type: .system)
         button.setTitle("NEXT", for: .normal)
         button.setTitleColor(UIColor.mainPink, for: .normal)
         button.translatesAutoresizingMaskIntoConstraints = false
         button.titleLabel?.font = UIFont.boldSystemFont(ofSize: 14)
-    button.addTarget(self, action: #selector(HandleNext), for: UIControlEvents.touchUpInside)
+        button.addTarget(self, action: #selector(HandleNext), for: UIControlEvents.touchUpInside)
         return button
     }()
     
+    @objc func HandlePrev()
+    {
+        
+        let prevpage : Int = max (pageController.currentPage - 1 ,0)
+        let indexpath = IndexPath(item: prevpage, section: 0)
+        pageController.currentPage = prevpage
+        print(pageController.currentPage)
+        collectionView?.scrollToItem(at: indexpath, at: .centeredHorizontally, animated: true)
+    }
     
     @objc func HandleNext()
     {
@@ -58,11 +57,16 @@ class SwipingController : UICollectionViewController,UICollectionViewDelegateFlo
         let indexpath = IndexPath(item: nextpage, section: 0)
         pageController.currentPage = nextpage
         collectionView?.scrollToItem(at: indexpath, at: .centeredHorizontally, animated: true)
+        if (pageController.currentPage > 0)
+        {
+            previousButton.isEnabled = true
+            previousButton.setTitleColor(UIColor.darkGray, for: .normal)
+            
+        }
     }
     
     override func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
         let x = targetContentOffset.pointee.x
-        //print(x,view.frame.width, x / view.frame.width)
         pageController.currentPage = Int(x / view.frame.width)
     }
     
@@ -95,7 +99,20 @@ class SwipingController : UICollectionViewController,UICollectionViewDelegateFlo
         collectionView?.backgroundColor = .white
         collectionView?.register(PageCell.self, forCellWithReuseIdentifier: "cellId")
         collectionView?.isPagingEnabled = true
-        
+    }
+    
+    override func collectionView(_ collectionView: UICollectionView, willDisplay cell: UICollectionViewCell, forItemAt indexPath: IndexPath) {
+        if indexPath.row == 0 {
+            previousButton.isEnabled = false
+            previousButton.setTitleColor(UIColor.lightGray, for: .normal)
+
+        }
+        else
+        {
+            previousButton.isEnabled = true
+            previousButton.setTitleColor(UIColor.darkGray, for: .normal)
+        }
+
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout,minimumLineSpacingForSectionAt section: Int) -> CGFloat {
